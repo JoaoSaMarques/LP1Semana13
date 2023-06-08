247bf27dca0a05ae36bc92da25335fd68ec617a6
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-06-08 13:33:24 +0100

    get min max 2

--- MultipleStuff/SuperList.cs
+++ MultipleStuff/SuperList.cs
@@ -23,5 +23,17 @@ namespace MultipleStuff
                     max = number;
             }
         }
+
+        /// <summary>
+        /// Get min max2
+        /// </summary>
+        /// <param name="min"></param>
+        /// <param name="GetMinMax2("></param>
+        /// <returns></returns>
+        public (double min, double max) GetMinMax2()
+        {
+            double min = double.MaxValue;
+            double max = double.MinValue;
+        }
     }
 }
