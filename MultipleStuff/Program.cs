b37568f25332b68b1bc52e41f29714eca86ca231
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-06-08 14:59:58 +0100

    Version 1

--- MultipleStuff/Program.cs
+++ MultipleStuff/Program.cs
@@ -7,7 +7,14 @@ namespace MultipleStuff
         static void Main(string[] args)
         {
             //Number list
-            SuperList numbers = new SuperList { 5.7, 2.9, 10.3, 1.5, 8.2 };
+            SuperList numbers = new SuperList { 1.2, 2.3, 4.5, 6.7, 8.9 };
+
+            //Version 1
+            double min1, max1;
+            //This will test Getminmax1
+            numbers.GetMinMax1(out min1, out max1);
+            Console.WriteLine("Versão 1:");
+            numbers.PrintMinMax(min1, max1);
         }
     }
 }
